fbb3a2f6bbba37805a385cd70b1601573f55cc7c
hassanraja2 <71331663+hassanraja2@users.noreply.github.com>
2020-09-30 16:14:32 +0100

    Update uploadmysql.sh

--- uploadmysql.sh
+++ uploadmysql.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo docker tag trio-task_myysql:latest localhost:8082/mysql:latest
+sudo docker tag trio-task_mysql:latest localhost:8082/mysql:latest
 sudo docker push localhost:8082/mysql:latest
